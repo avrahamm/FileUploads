f8526e87b210e21a4d6d6d96626713e033c0a1ee
avrahamm <papaabramg@gmail.com>
2019-06-03 19:07:56 +0300

    ewave exam - fixing migrations issue for mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fixing migrations issue for mysql
+        //@link:https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
     }
 }
